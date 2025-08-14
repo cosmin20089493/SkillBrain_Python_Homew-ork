f6923b237668dc0c3959124bb3e1c551d1c6a669
cosmin20089493e <cosmin20089493@gmail.com>
2025-08-14 11:11:06 +0200

    tema_3.py

---
+++ tema_2/major_sau_minor.py
@@ -0,0 +1,19 @@
+#varsta=int(input("Ce varsta aveti? "))
+#if varsta>=18:
+#    print("Sunteti major")
+#else:
+#    print("Sunteti minor")
+
+    #determinam ce anotimp este in functie de luna introdusa de utilizator
+
+luna_anului=int(input("In ce luna suntem?:"))
+if luna_anului>=2:
+    print("este iarna")
+elif 3<=luna_anului>=5:
+    print("este primavara")
+elif 6<=luna_anului>=8
+    print("este vara")
+elif 9<=luna_anului>=11
+    print(este toamna)
+else:
+    print(este iarna)
